93538a30161a245e59cd3fa438e9e214879ae0ec
YanicB <yanic.ballez@gmail.com>
2025-12-22 02:05:21 -0500

    add search bar

--- b_m_a/project/src/pages/tools/MindMapDashboard.jsx
+++ b_m_a/project/src/pages/tools/MindMapDashboard.jsx
@@ -1,6 +1,24 @@
+import { Network } from 'lucide-react';
+import { useState } from 'react';
+
 const MindMapDashboard = () => {
+    const [search, setSearch] = useState(''); 
+
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+    }
+
     return (
-        <div>Dashboard</div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-4">
+                    <Network className="h-8 w-8 text-green-600" />
+                    <h1 className="text-2xl font-bold text-gray-900">Mind Maps</h1>
+                </div>
+                <input placeholder="Search by title" type="text" value={search} onChange={handleChange}/>
+                <button>New</button>
+            </div>
+        </div>
     )
 }
 
