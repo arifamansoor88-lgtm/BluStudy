e49c5cf0c5510d7c8ef3c7ffc531cdcf0c7a83af
YanicB <yanic.ballez@gmail.com>
2025-12-26 17:45:46 -0500

    add delete mindmap functionality

--- b_m_a/project/src/pages/tools/Mindmaps/MindMapDashboard.jsx
+++ b_m_a/project/src/pages/tools/Mindmaps/MindMapDashboard.jsx
@@ -3,16 +3,15 @@ import { useState, useEffect, useCallback } from 'react';
 import { getMindmaps, deleteMindmap } from '../../../api/apiService';
 import { useMsal, useIsAuthenticated } from '@azure/msal-react';
 import { InteractionStatus } from '@azure/msal-browser';
+import ConfirmModal from './ConfirmModal';
 
 const MindMapDashboard = () => {
     const { instance, inProgress } = useMsal();
     const isAuthenticated = useIsAuthenticated();
     const [search, setSearch] = useState('');
     const [savedMindmaps, setSavedMindmaps] = useState([]);
-
-    const handleChange = (event) => {
-        const response = setSearch(event.target.value);
-    }
+    const [selectedMindmap, setSelectedMindmap] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
 
     // Search Logic to search through saved mindmaps
     const searchFilter = savedMindmaps.filter(mindmap => mindmap.data?.title.toLowerCase().includes(search.toLowerCase()));
@@ -26,6 +25,23 @@ const MindMapDashboard = () => {
             console.error("Fetch Failed.", err);
         }
     }, []);
+
+    const handleDeleteClick = (mindmap) => {
+        setOpenModal(true);
+        setSelectedMindmap(mindmap);
+    }
+
+    const handleConfirmDelete = async () => {
+        try {
+            await deleteMindmap(selectedMindmap.id);
+            setSavedMindmaps(savedMindmaps.filter(mindmap => mindmap.id !== selectedMindmap.id));
+        } catch (err) {
+            console.err("Error deleting mindmap.");
+        } finally {
+            setOpenModal(false);
+            setSelectedMindmap(null);
+        }
+    }
 
   // Load saved mindmaps on component mount
   useEffect(() => {
@@ -49,7 +65,7 @@ const MindMapDashboard = () => {
                         placeholder="Search by title"
                         type="text"
                         value={search}
-                        onChange={handleChange} />
+                        onChange={(e) => setSearch(e.target.value)} />
                 </div>
                 <button className="rounded-lg border-1 border-gray-200 px-4 py-2 bg-white text-gray-900 text-bold shadow-lg">Filter</button>
                 <button className="rounded-lg px-6 py-2 bg-blue-500 text-white text-bold shadow-lg hover:bg-blue-600">+ New Board</button>
@@ -65,17 +81,18 @@ const MindMapDashboard = () => {
                                 {mindmap.data?.title}
                             </h1>
                             <div className="flex gap-2">
-                                <button>
-                                    <Trash className="text-gray-400"/>
+                                <button onClick={() => handleDeleteClick(mindmap)}>
+                                    <Trash className="text-gray-400 rounded-lg p-1 hover:bg-red-50 w-6 h-6"/>
                                 </button>
                                 <button>
-                                    <MoreVertical className="text-gray-400"/>
+                                    <MoreVertical className="text-gray-400 w-4 h-4"/>
                                 </button>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
+            {openModal && <ConfirmModal setOpenModal={setOpenModal} onConfirm={handleConfirmDelete}/>}
         </div>
     )
 }
